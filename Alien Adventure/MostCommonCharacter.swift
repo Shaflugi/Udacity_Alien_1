769c827b113045fdec1ec962733ea61a8f5112ab
Matthew Marsalko <matthewmarsalko@Matthews-MacBook-Pro.local>
2016-02-20 19:32:12 -0800

    Finish most common character exercise

--- Alien Adventure/MostCommonCharacter.swift
+++ Alien Adventure/MostCommonCharacter.swift
@@ -9,6 +9,28 @@
 extension Hero {
     
     func mostCommonCharacter(inventory: [UDItem]) -> Character? {
+        var charList = [Character:Int]()
+        
+        // Pump all characters into a dictionary
+        for item in inventory{
+            for char in item.name.lowercaseString.characters{
+                if let _ = charList[char]{ // Char already in the list? Increment its value.
+                    charList[char]?++
+                }
+                else{ // Char not in the list? Initialize.
+                    charList[char] = 0
+                }
+            }
+        }
+        
+        // Sort the list by value
+        let sortedChars = charList.sort { $0.1 > $1.1 }
+    
+        // Return the highest valued character
+        if let highestChar = sortedChars.first?.0{
+            return highestChar
+        }
+        
         return nil
     }
 }
